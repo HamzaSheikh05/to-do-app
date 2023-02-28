4a439b3e40dc773b57b182584e2ec0a155b7d794
HamzaSheikh05 <hrsheikh02@gmail.com>
2023-02-28 22:26:05 +0400

    base ui completed with hardcoded data

--- src/components/DisplayTask.jsx
+++ src/components/DisplayTask.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
 export const DisplayTask = () => {
+  const tasks = [
+    { id: 1, name: "Task 1", time: "2:09:02 AM 28/02/2023" },
+    { id: 2, name: "Task 2", time: "3:00:02 AM 28/02/2023" },
+    { id: 3, name: "Task 3", time: "4:00:02 AM 28/02/2023" },
+    { id: 4, name: "Task 4", time: "5:09:02 AM 28/02/2023" },
+    { id: 5, name: "Task 5", time: "7:10:02 AM 28/02/2023" },
+  ];
   return (
     <section className="showTask">
       <div className="head">
@@ -11,14 +18,16 @@ export const DisplayTask = () => {
         <button className="clearAll">Delete All</button>
       </div>
       <ul>
-        <li>
-          <p>
-            <span className="name">Task A</span>
-            <span className="time">2:09:02 AM 28/02/2023</span>
-          </p>
-          <i class="bi bi-pencil-square"></i>
-          <i class="bi bi-trash"></i>
-        </li>
+        {tasks.map((task) => (
+          <li>
+            <p>
+              <span className="name">{task.name}</span>
+              <span className="time">{task.time}</span>
+            </p>
+            <i class="bi bi-pencil-square"></i>
+            <i class="bi bi-trash"></i>
+          </li>
+        ))}
       </ul>
     </section>
   );

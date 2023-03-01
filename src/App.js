bf5fb850bc82292ba3029b6f7825e9ca560dcf58
HamzaSheikh05 <hrsheikh02@gmail.com>
2023-03-01 11:55:02 +0400

    save tasks on local storage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Header } from "./components/Header";
 import { AddTask } from "./components/AddTask";
 import { DisplayTask } from "./components/DisplayTask";
 
 function App() {
-  const [listTask, setListTask] = useState([]);
+  const [listTask, setListTask] = useState(
+    JSON.parse(localStorage.getItem("listTask")) || []
+  );
   const [task, setTask] = useState({});
+
+  useEffect(() => {
+    localStorage.setItem("listTask", JSON.stringify(listTask));
+  }, [listTask]);
+
   return (
     <div className="App">
       <Header />
